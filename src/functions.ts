fbf614adbd41a5370472ac360fa4a263a8adbdd0
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-06-01 00:40:22 -0300

    Functions

---
+++ src/functions.ts
@@ -0,0 +1,19 @@
+const sum = (x: number, y: number): number => {
+    return x + y;
+}
+
+function mutiply(x: number, y: number): number => {
+    return x * y;
+}
+
+// let add = (x: number, y: number): number => {
+//     return x + y;
+// };
+
+// Com corpo reduzido
+let add = (x: number, y: number): number => x + y;
+
+
+const log = (message: string): void => {
+    console.log(message);
+}
