17401cb3f477a8a2dd99a61ea8a1bcc73c45a9dc
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-06-02 01:37:32 -0300

    Interfaces + funções

--- src/functions.ts
+++ src/functions.ts
@@ -17,3 +17,11 @@ let add = (x: number, y: number): number => x + y;
 const log = (message: string): void => {
     console.log(message);
 }
+
+interface mathFunc {
+    (x: number, y: number): number,
+}
+
+const subtract: mathFunc = (x: number, y: number) => {
+    return x - y;
+}
--- src/objects.ts
+++ src/objects.ts
@@ -7,6 +7,7 @@ type User = {
     email: string,
     password: string
     orders: Order[];
+    register(): string
 }
 
 type Order = {
@@ -27,5 +28,54 @@ const user: User = {
     gender: Gender.MALE,
     email: "luizotavio@gmail.com",
     password: "pswd",
-    orders: [{productId: 1, price: 119.00, quantity: 2}];
+    orders: [{productId: 1, price: 119.00, quantity: 2}],
+    register() {
+        return "2";
+    }
 };
+
+
+console.log(user.lastName); // propriedade nula, mas e aí?
+
+const printLog = (message: string) => {}
+
+printLog(user.lastName!);
+
+// Unions
+
+type Author = {
+    books: string[]
+};
+
+const author: Author & User = {
+    age: 21,
+    books: ["1"],
+    email: "author@gmail.com",
+    firstName: "Luiz",
+    orders: [],
+    register() {
+      return "a";
+    },
+  };
+
+// Interfaces
+
+interface UserInterface {
+    readonly firstName: string; // readonly a partir da instanciação se torna imutável, não poderá ser alterado.
+    email: string;
+}
+
+const emailUser: UserInterface = {
+    email: "luizotavio@gmail.com",
+    firstName: "Luiz"
+}
+
+interface AuthorInterface {
+    books: string[]
+}
+
+const newAuthor: UserInterface & AuthorInterface = {
+    email: "otavio@gmail.com",
+    firstName: "Otávio",
+    books: []
+}
